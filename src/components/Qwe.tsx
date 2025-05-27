b0bcad330bd154c2957c6f9c08300bd454810c80
Stanislav Zatolokin <szatolokin.web@gmail.com>
2025-05-27 19:24:57 +0300

    Add Qwe component

---
+++ src/components/Qwe.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export function Qwe() {
+  const [isFlag, setIsFlag] = useState(false);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setIsFlag((prevIsFlag) => !prevIsFlag);
+        }}
+      >
+        qwe {isFlag ? 0 : 1}
+      </button>
+      {isFlag ? <div>ewq</div> : null}
+    </div>
+  );
+}
