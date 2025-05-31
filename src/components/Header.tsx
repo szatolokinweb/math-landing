e618b01b086f9cea3c67878520c352c07a4c6b70
Stanislav Zatolokin <szatolokin.web@gmail.com>
2025-05-31 17:37:15 +0300

    Обновление

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,34 @@
+import Logo from "./Logo";
+import Nav, { NavLink } from "./Nav";
+
+export const navLinks: NavLink[] = [
+  {
+    href: "/me",
+    title: "Обо мне",
+  },
+  {
+    href: "/ege",
+    title: "ЕГЭ",
+  },
+  {
+    href: "/oge",
+    title: "ОГЭ",
+  },
+  {
+    href: "/documents",
+    title: "Материалы",
+  },
+];
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="p-5">
+      <div className="flex justify-center">
+        <Logo />
+      </div>
+      <div className="m-5">
+        <Nav links={navLinks} />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Link from "next/link";
+
+export type NavLink = {
+  href: string;
+  title: string;
+};
+
+export default function Nav({ links }: { links: NavLink[] }) {
+  return (
+    <div className="flex flex-col gap-2 items-center">
+      {links.map(({ href, title }, index) => (
+        <Link key={index} href={href}>
+          {title}
+        </Link>
+      ))}
+    </div>
+  );
+}
