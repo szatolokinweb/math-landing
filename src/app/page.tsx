0e2489248e4735bebefdacd1645cba904f69f070
Stanislav Zatolokin <szatolokin.web@gmail.com>
2025-05-31 17:13:23 +0300

    Обновление

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import { Qwe } from "@/components/Qwe";
-
 export default function Home() {
-  return (
-    <div>
-      <Qwe />
-      <Qwe />
-      <Qwe />
-      <div className="flex justify-end">qwe</div>
-    </div>
-  );
+  return <div>Page</div>;
 }
