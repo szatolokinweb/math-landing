1d68887377a62bf0f32ec53f55b0f0c9a42d98d2
Stanislav Zatolokin <szatolokin.web@gmail.com>
2025-06-02 01:13:11 +0300

    Обновление

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,14 +8,14 @@ export default function Nav({ links }: { links: NavLink[] }) {
   const pathname = usePathname();
 
   return (
-    <div className="flex flex-col gap-1 items-center">
+    <div className="flex flex-col gap-0.5 items-center">
       {links.map(({ title, url }, index) => {
         const isActive = pathname === url.pathname;
         return (
           <Link 
             key={index} 
             href={url.pathname + (url.hash ? `#${url.hash}` : '')}
-            className={`px-3 py-1 rounded-md transition-colors ${
+            className={`px-2.5 py-0.5 text-[0.95em] rounded-md transition-colors ${
               isActive 
                 ? 'bg-red-100 text-red-700 font-medium' 
                 : 'hover:bg-gray-100 text-gray-700'
