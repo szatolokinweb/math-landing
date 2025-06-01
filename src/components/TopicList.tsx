579748a10702a90bdf8d7a7c43c68358ffd4bdfd
Stanislav Zatolokin <szatolokin.web@gmail.com>
2025-06-01 22:45:02 +0300

    Обновление

--- src/components/TopicList.tsx
+++ src/components/TopicList.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 interface Topic {
   name: string;
   pdfUrl: string;
@@ -16,14 +18,14 @@ export const TopicList = ({ title, topics, className = '' }: TopicListProps) => 
       <ul className="list-disc list-inside space-y-1 text-gray-600">
         {topics.map((topic, index) => (
           <li key={index}>
-            <a 
+            <Link 
               href={topic.pdfUrl}
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-600 hover:text-blue-800 hover:underline transition-colors"
             >
               {topic.name}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
