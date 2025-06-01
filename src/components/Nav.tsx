44bd082aa360561c4045d33aedcff26e930af4d3
Stanislav Zatolokin <szatolokin.web@gmail.com>
2025-06-01 18:13:30 +0300

    Обновление

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,8 +4,8 @@ import { NavLink } from "@/utils/NavLink";
 export default function Nav({ links }: { links: NavLink[] }) {
   return (
     <div className="flex flex-col gap-1 items-center">
-      {links.map(({ href, title }, index) => (
-        <Link key={index} href={href}>
+      {links.map(({ title, url }, index) => (
+        <Link key={index} href={url}>
           {title}
         </Link>
       ))}
--- src/utils/NavLink.tsx
+++ src/utils/NavLink.tsx
@@ -1,23 +1,66 @@
 export type NavLink = {
-  href: string;
   title: string;
+  url: {
+    pathname: string;
+    hash?: string;
+  };
 };
 
 export const globalNavLinks: NavLink[] = [
   {
-    href: "/",
-    title: "Обо мне",
-  },
-  {
-    href: "/ege",
     title: "ЕГЭ",
+    url: {
+      pathname: "/ege",
+    },
   },
   {
-    href: "/oge",
     title: "ОГЭ",
+    url: {
+      pathname: "/oge",
+    },
   },
   {
-    href: "/documents",
+    title: "5-8",
+    url: {
+      pathname: "/5-8",
+    },
+  },
+  {
+    title: "Олимпиады",
+    url: {
+      pathname: "/olympic",
+    },
+  },
+  {
+    title: "ДВИ",
+    url: {
+      pathname: "/dvi",
+    },
+  },
+  {
     title: "Материалы",
+    url: {
+      pathname: "/worksheets",
+    },
+  },
+  {
+    title: "Обо мне",
+    url: {
+      pathname: "/",
+      hash: "about",
+    },
+  },
+  {
+    title: "Документы",
+    url: {
+      pathname: "/documents",
+    },
+  },
+  {
+    title: "Контакты",
+    url: {
+      pathname: "/",
+      hash: "contacts",
+    },
   },
 ];
